9fec8d3afb203a9567f8781d5006b6119f5a01bc
theVersan <158575755+daviversan@users.noreply.github.com>
2025-02-18 19:18:48 -0300

    v10

--- angular_app/src/app/portfolio/portfolio.component.ts
+++ angular_app/src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,6 +10,17 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: 'Sample Angular App',
+    summary: 'Summary',
+    description: '',
+    projectLink: '',
+    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
+    pictures: []
+
+  };
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Davi Versan - Portfolio');
