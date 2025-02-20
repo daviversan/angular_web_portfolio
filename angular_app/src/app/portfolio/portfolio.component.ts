6cec27147fb4fcf58debefa56b4814f18a3abf1f
theVersan <158575755+daviversan@users.noreply.github.com>
2025-02-20 10:55:18 -0300

    v13

--- angular_app/src/app/portfolio/portfolio.component.ts
+++ angular_app/src/app/portfolio/portfolio.component.ts
@@ -14,10 +14,46 @@ export class PortfolioComponent implements OnInit {
 
   projects = [] as Project[];
 
+  isCollapsed: boolean = true;
+  typescript: boolean = false;
+  angular: boolean = false;
+  filtering: boolean = false;
+
+
   constructor(private titleService: Title, private projectService: ProjectsService) {
     this.titleService.setTitle('Davi Versan - Portfolio');
   }
   ngOnInit(): void {
+    this.projects = this.projectService.GetProjects();
+  }
+
+  Filter() {
+    let filterTags: Tag[] = [];
+
+    if (this.typescript) {
+      filterTags.push(Tag.TYPESCRIPT);
+    }
+
+    if (this.angular) {
+      filterTags.push(Tag.ANGULAR);
+    }
+
+    if (this.typescript || this.angular) {
+      this.filtering = true;
+    }
+    else {
+      this.filtering = false;
+    }
+
+    
+    this.projects = this.projectService.GetProjectsByFilter(filterTags);
+  }
+
+  ResetFilters() {
+    this.typescript = false;
+    this.angular = false;
+    this.filtering = false;
+
     this.projects = this.projectService.GetProjects();
   }
   
